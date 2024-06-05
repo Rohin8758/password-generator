f952c274a07d04e03aabea3566670fc5c7ef331d
Rohin8758 <rohinbambhroliya8758@gmail.com>
2024-06-05 10:04:48 +0530

    first commit

---
+++ src/Other.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Other({initialCount}) {
+    const [count, setCount] = useState(initialCount);
+    return (
+      <>
+        Count: {count}
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      </>
+    );
+  }
+  export default Other;
